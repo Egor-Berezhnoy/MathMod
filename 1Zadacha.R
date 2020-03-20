4c6db966205c332738d7d75552974034c4d39689
Egor-Berezhnoy <60848332+Egor-Berezhnoy@users.noreply.github.com>
2020-03-20 17:09:45 +0300

    Update 1Zadacha.R

--- 1Zadacha.R
+++ 1Zadacha.R
@@ -8,6 +8,7 @@ library (rnoaa)
 #Скачиваем список метеостанций
 station_data=ghcnd_stations()
 write.csv(station_data,"station_data20.csv")
+station_data = read.csv("station_data20.csv")
 #Формирование списка метеостанций
 lipetsk = data.frame(id = "LIPETSK", latitude = 52.6031,  longitude = 39.5708)
 #Задание временного периода и необходимых переменных 
@@ -21,7 +22,7 @@ summary (lipetsk_table)
 #Создание цикла, в котором скачиваюсь необходимые данные с метеостанций
 #Промежуточный объект куда скачиваются данные с конкретной метеостанции
 all_i = data.frame()
-#Объект куда скачиваются все данные всех метеостанций
+#Объкт куда скачиваются все данные всех метеостанций
 all_lipetsk_meteodata = data.frame()
 #Создание цикла для всех метеостанций
 for(i in 1:8)
@@ -29,6 +30,7 @@ for(i in 1:8)
 all_i=meteo_tidy_ghcnd(stationid =lipetsk_id, var="TAVG", date_min = "2005-01-01", date_max = "2015-12-31") 
 all_lipetsk_meteodata=rbind(all_lipetsk_meteodata, all_i)
 }
+#Запись полученных данных в файл
 write.csv (all_lipetsk_meteodata,"all_lipetsk_meteodata.csv")
 all_lipetsk_meteodata
   
